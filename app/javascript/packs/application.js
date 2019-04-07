ebede6615e870aabbf4ea80b1ed7065c75cc1679
Guilherme Yamakawa de Oliveira <guilherme.ymk@gmail.com>
2019-04-07 12:15:59 -0300

    configurando o ReactJS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,6 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+const componentRequireContext = require.context('components', true)
+const ReactRailsUJS = require('react_ujs')
+ReactRailsUJS.useContext(componentRequireContext)
